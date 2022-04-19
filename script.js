11a5dd4d0418bf973e1c7c74c1169b6728296504
Emma Gerigscott <elgerig@gmail.com>
2022-04-18 17:37:58 -0700

    Need to figure out how to call on createGrid function...

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
+const container = document.querySelector('.grid-container');
 let gridSize = 16;
 
 function createGrid(gridSize) {
-    const container = document.querySelector('.grid-container');
-    
     for (let i=0; i < gridSize; i++) {
         const squares = document.createElement('div');
         squares.classList.add('squares');
-        squares.style = ('backgroundColor: red;')
         container.appendChild(squares);
     }
-}
+} 
