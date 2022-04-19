89c062d759cbe75852643e35f240eded4d28c4b6
Emma Gerigscott <elgerig@gmail.com>
2022-04-18 17:04:43 -0700

    more unsuccessful attempts at getting a grid to appear...

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const container = document.querySelector('.grid-container');
+
 let gridSize = 16;
 
 function createGrid(gridSize) {
-    gridSize.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-    gridSize.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+    const container = document.querySelector('.grid-container');
     
-    for (let i=0; i < gridSize * gridSize; i++) {
+    for (let i=0; i < gridSize; i++) {
         const squares = document.createElement('div');
         squares.classList.add('squares');
+        squares.style = ('backgroundColor: red;')
         container.appendChild(squares);
     }
 }
