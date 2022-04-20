9a1655d8c3e4b436cbec73584607f654e7f5227a
Emma Gerigscott <elgerig@gmail.com>
2022-04-19 17:39:17 -0700

    Establish createGrid, hover add button

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const container = document.querySelector('.grid-container');
 let gridSize = 16;
 
-function createGrid(gridSize) {
-    for (let i=0; i < gridSize; i++) {
+function createGrid() {
+container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+container.style.gridTemplateRows= `repeat(${gridSize}, 1fr)`;
+    for (let i=0; i < gridSize * gridSize; i++) {
         const squares = document.createElement('div');
         squares.classList.add('squares');
+        squares.addEventListener('mouseover', changeColor);
         container.appendChild(squares);
     }
-} 
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'lightpink'
+}
+createGrid()
