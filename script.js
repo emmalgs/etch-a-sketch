cbbabc64e4db78dce4c48dccdb03423cee9c87c2
Emma Gerigscott <elgerig@gmail.com>
2022-04-21 14:50:07 -0700

    Input form recieves number and generates new grid! Yay!

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
 const container = document.querySelector('.grid-container');
 const clearGrid = document.querySelector('#clearGrid');
 const gridValue = document.querySelector('.gridValue');
-const defaultSize = 16
 
-let gridSize = defaultSize;
+document.querySelector('#submit').addEventListener('click', function(event) {
+    gridSize = document.getElementById('gridInput').value
+    createGrid(gridSize);
+    event.preventDefault();
+})
 
-gridValue.textContent = `${gridSize} x ${gridSize}`;
+clearGrid.addEventListener('click', () => {
+    window.setTimeout(() => {
+        window.location.reload(true);
+    }, 200);
+});
 
-function reloadGrid() {
-    clearGrid.addEventListener('click', () => {
-        window.setTimeout(() => {
-            window.location.reload(true);
-        }, 200);
-    });
-    let value = prompt(`Enter a new grid size`);
-    if (value > 100) {
-        return `Enter a smaller number`
-    } else if (value === Number) {
-        let gridSize = value
-        createGrid(gridSize)
-    }
-
-}
-
-reloadGrid()
-
-function createGrid() {
+function createGrid(gridSize) {
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 container.style.gridTemplateRows= `repeat(${gridSize}, 1fr)`;
     for (let i=0; i < gridSize * gridSize; i++) {
@@ -40,5 +29,3 @@ container.style.gridTemplateRows= `repeat(${gridSize}, 1fr)`;
 function changeColor(e) {
     e.target.style.backgroundColor = 'lightpink'
 }
-
-createGrid()
